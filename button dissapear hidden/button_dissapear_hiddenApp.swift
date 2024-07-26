944bda677066942a2fb50d8ec0d89ef811df7b20
pearl0nyak <pearlnyax@gmail.com>
2024-07-26 13:23:12 +0100

    Initial Commit

---
+++ button dissapear hidden/button_dissapear_hiddenApp.swift
@@ -0,0 +1,17 @@
+//
+//  button_dissapear_hiddenApp.swift
+//  button dissapear hidden
+//
+//  Created by Scholar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct button_dissapear_hiddenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
